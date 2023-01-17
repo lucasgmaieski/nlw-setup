6495b96917aa17a39eead51b73a20cde68251f25
Lucas Maieski <lucasgrigol@gmail.com>
2023-01-17 19:22:04 -0300

    instalando dependencias e configurações iniciais

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+/*
+* Método HTTP: Get, Post, Put, Patch, Delete
+*/
+
+app.get('/', ()=> {
+    return 'Hello World'
+})
+
+app.listen({
+    port: 3333,
+}).then(()=> {
+    console.log('HTTP Server running!')
+})
