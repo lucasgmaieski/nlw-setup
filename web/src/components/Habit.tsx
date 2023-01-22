ac0fde6a99d79cdc0cbe51aab6fe4a0ea094075f
Lucas Maieski <lucasgrigol@gmail.com>
2023-01-22 19:31:54 -0300

    criando componente de loading e definindo os icones

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -4,6 +4,6 @@ interface HabitProps {
 
 export function Habit(props: HabitProps ) {
     return (
-        <div className="bg-zinc-900 w-10 h-10 text-white">{props.completed}</div>
+        <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center">{props.completed}</div>
     )
 }
